20222ad9505ca29ecc99790145e581b4fe21bac5
williamchidi <williamchidi1@gmail.com>
2024-03-02 19:00:01 +0100

    updated

--- script.js
+++ script.js
@@ -4,18 +4,87 @@ class Telephone {
   constructor() {
     this.numbers = new Set();
     this.observers = [];
+    this.inputValue = '';
+
+    this.modal = document.querySelector('.modal');
+    this.outPut = document.querySelector('.text');
+    this.icon = document.querySelector('.icon');
+
+    this.addBtn = document.querySelector('.add');
+    this.dialBtn = document.querySelector('.dialing');
+    this.delBtn = document.querySelector('.delete');
+    this.info = document.querySelector('.small');
+    this.input = document.getElementById('input');
+
+    this.icon.addEventListener('click', () => this.toggleModal());
+
+    this.input.addEventListener('focus', () => {
+      if (this.modal.classList.contains('show-modal')) {
+        this.input.disabled = true;
+        this.info.style.display = 'flex';
+        this.addBtn.disabled = true;
+      }
+    });
+
+    this.addBtn.addEventListener('click', () => {
+      this.inputValue = this.input.value;
+      if (this.inputValue.trim() === '') {
+        this.info.style.display = 'flex';
+        return (this.info.textContent =
+          'Please input phone number you want to Add');
+      }
+      this.addPhoneNumber(this.inputValue);
+      this.toggleModal();
+      this.input.value = '';
+      console.log(this.input);
+    });
+
+    this.delBtn.addEventListener('click', () => {
+      this.inputValue = this.input.value;
+      if (this.inputValue === '') {
+        this.info.style.display = 'flex';
+        return (this.info.textContent =
+          'Please input phone number you want to Delete');
+      }
+      this.removePhoneNumber(this.inputValue);
+      this.toggleModal();
+      this.input.value = '';
+    });
+
+    this.dialBtn.addEventListener('click', () => {
+      this.inputValue = this.input.value;
+      if (this.inputValue === '') {
+        this.info.style.display = 'flex';
+        return (this.info.textContent =
+          'Please input phone number you want to dial');
+      }
+    });
   }
 
   addPhoneNumber(num) {
-    this.numbers.add(num);
+    const phoneNumber = parseInt(num);
+    this.numbers.add(phoneNumber);
+    this.outPut.textContent = `${num} have been added to phone book`;
+    console.log(this.numbers);
   }
 
   removePhoneNumber(num) {
-    this.numbers.delete(num);
+    const phoneNumber = parseInt(num);
+    if (!this.numbers.has(phoneNumber))
+      return (this.outPut.textContent = `${phoneNumber} is not saved on the telephone book`);
+
+    this.numbers.delete(phoneNumber);
+    this.outPut.textContent = 'Phone number successfully deleted';
+    console.log(this.numbers);
   }
 
   dialPhoneNumber(num) {
-    this.notifyObserver(num);
+    const phoneNumber = parseInt(num);
+    if (!this.numbers.has(phoneNumber)) {
+      this.info.style.display = 'flex';
+      return (this.info.textContent = `Dialed number (${phoneNumber}) is not included in your phone book`);
+    }
+    this.notifyObserver(phoneNumber);
   }
 
   addObserver(observer) {
@@ -29,6 +98,13 @@ class Telephone {
   notifyObserver(num) {
     this.observers.forEach((obs) => obs.notify(num));
   }
+
+  toggleModal() {
+    this.modal.classList.toggle('show-modal');
+    this.info.style.display = 'none';
+    this.input.disabled = false;
+    this.addBtn.disabled = false;
+  }
 }
 
 class DialedNumberObserver {
@@ -39,7 +115,7 @@ class DialedNumberObserver {
 
 class DialingNumberObserver {
   notify(num) {
-    console.log(`Dialing ${num}`);
+    console.log(`Dialing ${num}....`);
   }
 }
 
@@ -50,5 +126,5 @@ const dialedNumber = new DialedNumberObserver();
 telephone.addObserver(dialedNumber);
 telephone.addObserver(dialingNumber);
 
-telephone.addPhoneNumber('07033881174');
-telephone.addPhoneNumber('08163115022');
+// telephone.addPhoneNumber('07033881174');
+// telephone.addPhoneNumber('08163115022');
